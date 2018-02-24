c02d295c594696e61743029d5bd647e91651049d
Raf Castanier <raphael.castanier@free.fr>
2018-02-24 18:23:59 +0100

    Correction de la grille de base

--- vigenere.py
+++ vigenere.py
@@ -3,18 +3,26 @@
 
 import string
 
-from string import ascii_lowercase #liste des lettres de l'alphabet en minuscule
+from string import ascii_lowercase # liste des lettres de l'alphabet en minuscule
 
 def afficherGrille():
-    print(ascii_lowercase)
+
+    # première ligne
+    print ' ',
+    for l in ascii_uppercase :
+        print l.upper(),
+    print
+
+    # grille
     for cle in ascii_lowercase:
+        print cle.upper(), # premier caractère
         for lettre in ascii_lowercase:
-            print(coderLettre(cle, lettre), '', ' ')
-        print("\n")
+            print coderLettre(cle, lettre),
+        print
 
 def coderLettre(cle, lettre):
     index = string.find(ascii_lowercase, cle) + string.find(ascii_lowercase, lettre)
-    return ascii_lowercase[index]
+    return ascii_lowercase[index % len(ascii_lowercase)]
 
 if __name__ == "__main__":
     afficherGrille()
