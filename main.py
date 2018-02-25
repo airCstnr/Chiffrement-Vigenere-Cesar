198d6a40a976e245076638d0336b58af144a3312
Raf Castanier <raphael.castanier@free.fr>
2018-02-25 16:13:49 +0100

    Modification du main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from vigenere import testerVigenere
+from vigenere import *
 
 testerVigenere()
+
+# cle = "PASSION"
+# message_a_decoder = "Jn tsqgrg, msaa o gdul hzsasrw,"
+# print decoderMessage(cle, message_a_decoder)
