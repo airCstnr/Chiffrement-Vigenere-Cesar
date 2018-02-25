5f553f2e74c697b0e1d09f44a5cedd3061009863
Raf Castanier <raphael.castanier@free.fr>
2018-02-25 14:27:08 +0100

    Fin de l'implémentation des fonctions de Vigenere

--- vigenere.py
+++ vigenere.py
@@ -9,16 +9,59 @@ from string import ascii_lowercase # liste des lettres de l'alphabet en minuscul
 # FONCTIONS DE TEST
 # ----------------------------
 def testerVigenere():
+    print
     print "Grille de Vigenere :"
     afficherGrille()
     print
-    print "Test de Codage :"
-    testerCodage()
+    print "Test de Codage de lettre :"
+    testerCodageLettre()
     print
-    # print "Test de Décodage :"
-    # testerDecodage()
-    # print
+    print "Test de Decodage de lettre :"
+    testerDecodageLettre()
+    print
+    print "Test de Codage de message :"
+    testerCodageMessage()
+    print
+    print "Test de Decodage de message :"
+    testerDecodageMessage()
+    print
     print "Tests OK"
+
+def testerCodageLettre():
+    message_clair = "message clair"
+    cle = "cle"
+    for index in range(len(message_clair)):
+        lettre_claire = message_clair[index]
+        lettre_cle    = cle[index % len(cle)]
+        lettre_codee  = coderLettre(lettre_cle, lettre_claire)
+        print lettre_claire, lettre_cle, lettre_codee
+
+def testerDecodageLettre():
+    message_code = "opwulkg gnlmt"
+    cle = "cle"
+    for index in range(len(message_code)):
+        lettre_codee  = message_code[index]
+        lettre_cle    = cle[index % len(cle)]
+        lettre_claire = decoderLettre(lettre_cle, lettre_codee)
+        print lettre_codee, lettre_cle, lettre_claire
+
+def testerCodageMessage():
+    message_clair = "message clair"
+    cle = "cle"
+    print message_clair, " : ", message_clair
+    print cle, " : ", cle
+    print "message code : ", coderMessage(cle, message_clair)
+
+def testerDecodageMessage():
+    message_code = "opwulkg gnlmt"
+    cle = "cle"
+    print message_code, " : ", message_code
+    print cle, " : ", cle
+    print "message clair : ", decoderMessage(cle, message_code)
+
+# ----------------------------
+# FONCTIONS DE VIGENERE
+# ----------------------------
 
 def afficherGrille():
     # première ligne
@@ -33,43 +76,38 @@ def afficherGrille():
             print coderLettre(cle, lettre),
         print
 
-def testerCodage():
-    message_clair = "message clair"
-    cle = "cle"
-    for index in range(len(message_clair)):
-        lettre_claire = message_clair[index]
-        lettre_cle    = cle[index % len(cle)]
-        lettre_codee  = coderLettre(lettre_cle, lettre_claire)
-        print lettre_claire, lettre_cle, lettre_codee
-
-def testerCodage():
-    message_clair = "message clair"
-    cle = "cle"
-    for index in range(len(message_clair)):
-        lettre_claire = message_clair[index]
-        lettre_cle    = cle[index % len(cle)]
-        lettre_codee  = coderLettre(lettre_cle, lettre_claire)
-        print lettre_claire, lettre_cle, lettre_codee
-
-# ----------------------------
-# FONCTIONS DE VIGENERE
-# ----------------------------
 def coderLettre(cle, lettre):
     if string.find(ascii_lowercase, lettre) > -1:
-        index = string.find(ascii_lowercase, cle) + string.find(ascii_lowercase, lettre)
+        index = string.find(ascii_lowercase, lettre) + string.find(ascii_lowercase, cle)
         return ascii_lowercase[index % len(ascii_lowercase)]
     else:
         return lettre
 
 def decoderLettre(cle, lettre):
     if string.find(ascii_lowercase, lettre) > -1:
-        index = string.find(ascii_lowercase, cle) - string.find(ascii_lowercase, lettre)
+        index = string.find(ascii_lowercase, lettre) - string.find(ascii_lowercase, cle)
         return ascii_lowercase[index % len(ascii_lowercase)]
     else:
         return lettre
 
-# ----------------------------
-# UTL
+def coderMessage(cle, message_clair):
+    message_code = ''
+    for index in range(len(message_clair)):
+        lettre_claire = message_clair[index]
+        lettre_cle    = cle[index % len(cle)]
+        lettre_codee  = coderLettre(lettre_cle, lettre_claire)
+        message_code += lettre_codee
+    return message_code
+
+def decoderMessage(cle, message_code):
+    message_clair = ''
+    for index in range(len(message_code)):
+        lettre_codee  = message_code[index]
+        lettre_cle    = cle[index % len(cle)]
+        lettre_claire = decoderLettre(lettre_cle, lettre_codee)
+        message_clair += lettre_claire
+    return message_clair
+
 # ----------------------------
 if __name__ == "__main__":
     testerVigenere()
